aa07557414e7b5c7bfedae9ecbd63ba77374073a
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2022-03-16 18:33:31 -0500

    Backend api rest for android app for make pagination with kotlin and paging3 lib

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    private $NUM_PAGES = 15;
+
+    public function users(Request $request){
+
+        $nombre = $request->name;
+
+        $users = User::buscarPorNombre($nombre)->paginate($this->NUM_PAGES);
+        //retornar response json
+        return response()->json([
+             $users,
+        ], 200);
+
+    }
+}
